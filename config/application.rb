f1db736a44e7f48d04c0f79cac7cf2623805ff2c
Johnathan Chei <j.chei175@gmail.com>
2015-11-05 14:02:49 -0500

    added a line to config application file

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module Trippy
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.serve_static_assets = true
     config.active_record.raise_in_transactional_callbacks = true
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
   html_tag
